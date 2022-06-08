144808c23a00a56ba916cf6f43747f4a0e6b5609
Primasbt <91203079+primasbt@users.noreply.github.com>
2022-06-09 00:50:36 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Person person = new Person("Mike", 29);
+            person.GetNameAndAge();
+
+            Teacher tch = new Teacher("Thomas", 22, "209148", "Math");
+            tch.GetNameAndAge();
+
+            Student std = new Student("Sara", 19, "901341", "augag@gmail.com");
+            std.GetNameAndAge();
+        }
+    }
+}
